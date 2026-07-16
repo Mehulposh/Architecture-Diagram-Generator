9774c7e5d5bdd0927f6f915d4979cfe1360dd77f
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-07-16 09:44:19 +0530

    feat: include isAdmin field in user schema and update toSafeJSON method

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     passwordHash: { type: String, required: true },
+    isAdmin: {type: Boolean, default: false}
   },
   { timestamps: true }
 );
@@ -20,7 +21,7 @@ userSchema.methods.comparePassword = function comparePassword(password) {
 };
 
 userSchema.methods.toSafeJSON = function toSafeJSON() {
-  return { id: this._id, name: this.name, email: this.email, createdAt: this.createdAt };
+  return { id: this._id, name: this.name, email: this.email, isAdmin: this.isAdmin, createdAt: this.createdAt };
 };
 
 module.exports = mongoose.model('User', userSchema);
