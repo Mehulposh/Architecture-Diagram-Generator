d509fb717b4a8cb67e165151c09c69bf33e94f56
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-07-24 18:08:43 +0530

    feat: create Sidebar component to organize and export related panels

---
+++ client/src/components/sidebar/index.js
@@ -0,0 +1,11 @@
+export { default as Sidebar } from "../Sidebar";
+
+export { default as DomainPanel } from "./DomainPanel";
+
+export { default as ComponentPalette } from "./ComponentPalette";
+
+export { default as AIReviewPanel } from "./AIReviewPanel";
+
+export { default as TechStackPanel } from "./TechStackPanel";
+
+export { default as ReadOnlyBanner } from "./ReadOnlyBanner";
