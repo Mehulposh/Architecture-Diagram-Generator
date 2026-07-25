eba3a147fd2e49fea3819da3101ef45e7c3d95c0
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-07-25 20:03:35 +0530

    feat: add index file to export PromptBar, PromptInput, StyleSelector, GenerateButton, and PromptError components

---
+++ client/src/components/prompt/index.js
@@ -0,0 +1,5 @@
+export { default as PromptBar } from "./PromptBar";
+export { default as PromptInput } from "./PromptInput";
+export { default as StyleSelector } from "./StyleSelector";
+export { default as GenerateButton } from "./GenerateButton";
+export { default as PromptError } from "./PromptError";
