305b0ec566b84d52272093445de27dadf69f9442
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2026-07-25 20:05:11 +0530

    feat: create index file to export editor components

---
+++ client/src/components/editor/index.js
@@ -0,0 +1,13 @@
+export { default as Editor } from "./Editor";
+
+export { default as Workspace } from "./Workspace";
+
+export { default as CanvasContainer } from "./CanvasContainer";
+
+export { default as Footer } from "./Footer";
+
+export { default as EmptyState } from "./EmptyState";
+
+export { default as PermissionBanner } from "./PermissionBanner";
+
+export { default as LiveIndicator } from "./LiveIndicator";
